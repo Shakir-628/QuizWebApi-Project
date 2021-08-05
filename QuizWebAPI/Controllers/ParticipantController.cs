07a9e9b966d4dd1485a7d33ea0ca0d832344241b
Shakir <mshakir628@gmail.com>
2021-08-05 17:31:29 +0500

    Initial commit

---
+++ QuizWebAPI/Controllers/ParticipantController.cs
@@ -0,0 +1,38 @@
+﻿using QuizWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace QuizWebAPI.Controllers
+{
+    public class ParticipantController : ApiController
+    {
+        
+        [HttpPost]
+        [Route("api/InsertParticipant")]
+        public Participant Insert(Participant model)
+        {
+            using (DBModel db = new DBModel())
+            {
+                db.Participants.Add(model);
+                db.SaveChanges();
+                return model;
+            }
+              
+        }
+
+        [HttpPost]
+        [Route("api/UpdateParticipant")]
+        public void UpdateOutput(Participant model)
+        {
+            using (DBModel db = new DBModel())
+            {
+                db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ QuizWebAPI/Controllers/QuestionsController.cs
@@ -0,0 +1,55 @@
+﻿using QuizWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace QuizWebAPI.Controllers
+{
+    public class QuestionsController : ApiController
+    {
+        [HttpGet]
+        [Route("api/Questions")]
+        public HttpResponseMessage GetQuesitons(Question model)
+        {
+            using (DBModel db = new DBModel())
+            {
+                var Qns = db.Questions
+                .Select(x => new { QnID = x.QnID, Qn = x.Qn, ImageName = x.ImageName, option1 = x.Option1, option2 = x.Option2, option3 = x.Option3, option4 = x.Option4 })
+                .OrderBy(y => Guid.NewGuid())
+                .Take(10)
+                .ToArray();
+
+                var updated = Qns.AsEnumerable()
+                   .Select(x => new
+                   {
+                       QnID = x.QnID,
+                       Qn = x.Qn,
+                       ImageName = x.ImageName,
+                       options = new String[] { x.option1, x.option2, x.option3, x.option4 }
+                   }).ToList();
+
+                return this.Request.CreateResponse(HttpStatusCode.OK, updated);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("api/Answers")]
+
+        public HttpResponseMessage GetAnswers(int[] qIDs)
+        {
+            using (DBModel db = new DBModel())
+            {
+                var result = db.Questions.AsEnumerable()
+                .Where(y => qIDs.Contains(y.QnID))
+                .OrderBy(x => { return Array.IndexOf(qIDs, x.QnID); })
+                .Select(z => z.Answer)
+                .ToArray();
+                return this.Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+        }
+    }
+}
